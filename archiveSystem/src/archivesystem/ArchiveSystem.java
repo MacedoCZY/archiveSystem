c240131c28f25fb310da23cff78dba7b72e1565b
MacedoCZY <gustavomacedo1366@hotmail.com>
2023-07-02 11:10:00 -0300

    sec

--- archiveSystem/src/archivesystem/ArchiveSystem.java
+++ archiveSystem/src/archivesystem/ArchiveSystem.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package archivesystem;
+import java.io.*;
 
 /**
  *
@@ -15,6 +16,26 @@ public class ArchiveSystem {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+    }
+
+    public void formatDisc(String args) throws FileNotFoundException, IOException{
+        //abrir o arquivo em modo leitura e escrita, com base no args passado
+        RandomAccessFile acsFile = new RandomAccessFile(args, "rw");
+        
+        //instanciar a classe boot record
+        bootRecord btRec = new bootRecord();
+        
+        //definir o ponteiro do arquivo para o inicio
+        acsFile.seek(0);
+        
+        acsFile.writeShort(btRec.getSectorSize()[0]);
+        
+        acsFile.seek(1);
+ 
+        acsFile.writeShort(btRec.getSectorSize()[1]);
+
+        
     }
     
 }
--- archiveSystem/src/archivesystem/bootRecord.java
+++ archiveSystem/src/archivesystem/bootRecord.java
@@ -9,6 +9,43 @@ package archivesystem;
  * @author macedo
  */
 public class bootRecord {
-    public double sectorSize;
+    public short[] sectorSize = new short[2];
+    public byte sectorReserv;
+    public byte numFAT;
+    public byte entriesRootDir;
+    public int[] totalSectors = new int[4];
+    public byte[] ocpSectors = new byte[2];
+
+    public short[] getSectorSize() {
+        this.sectorSize[0] = 0;
+        this.sectorSize[1] = 8;
+        
+        return sectorSize;
+    }
+
+    public short getSectorReserv() {
+        return 1;
+    }
+
+    public short getNumFAT() {
+        return 1;
+    }
+
+    public short getEntriesRootDir() {
+        return 4;
+    }
+
+    public int[] getTotalSectors() {
+        this.totalSectors[0] = 40;
+        this.totalSectors[1] = 00;
+        this.totalSectors[2] = 01;
+        
+        return totalSectors;
+    }
+
+    public short getOcpSectors() {
+        return 66;
+    }
+    
     
 }
